564a043711edbc4dffa477ca18929d3542e9001a
RiccardoC <rcomo.2003@gmail.com>
2023-01-24 10:56:52 +0100

    prova

---
+++ src/Paroliere/Grafica.java
@@ -0,0 +1,12 @@
+package Paroliere;
+
+import javax.swing.*;
+
+public class Grafica {
+    private JTable table1;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
